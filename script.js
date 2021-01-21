2157afed31682ef155979bd7e66f7a4474f2042f
leorpedroso <leorpedroso97@gmail.com>
2021-01-20 23:14:24 -0300

    Add functionality for 'AC', '.' and '+/-' buttons

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-//Basic math functions
-const add = function addsTwoNumbers(x, y) {
+let display = '';
+
+// Basic math functions
+const add = function addTwoNumbers(x, y) {
   return x + y;
 }
 
-const subtract = function subtractsTwoNumbers(x, y) {
+const subtract = function subtractTwoNumbers(x, y) {
   return x - y;
 }
 
-const multiply = function multipliesTwoNumbers(x, y) {
+const multiply = function multiplyTwoNumbers(x, y) {
   return x * y;
 }
 
-const divide = function dividesTwoNumbers(x, y) {
+const divide = function divideTwoNumbers(x, y) {
   return x / y;
 }
 
-const operate = function callsBasicFunctions(operator, x, y) {
+const updateCalculatorDisplay = function updateCalculatorDisplay(newString = display) {
+  document.querySelector('.inputDisplay').textContent = newString;
+}
+
+const clearUserInputs = function clearCurrentDisplaySavedOperationAndStoredDisplay() {
+  display = '';
+  updateCalculatorDisplay('');
+}
+
+const invertDisplayValue = function toggleDisplayValueBetweenNegativeAndPositive() {
+  display.charAt(0) === '-' ? display = display.slice(1) : display = `-${display}`;
+  updateCalculatorDisplay();
+}
+
+const insertDecimalPoint = function insertDecimalPoint() {
+  if (!display.includes('.')) display = `${display}.`;
+  updateCalculatorDisplay();
+}
+
+const operate = function callCalculatorOperations(operator, x, y) {
   switch (operator) {
     case 'add':
       return add(x, y);
@@ -33,15 +54,54 @@ const operate = function callsBasicFunctions(operator, x, y) {
       break;
   }
 }
-//What do I actually want to do here?
-const verifyInput = function verifyTypeOfButtonPressed(buttonPressed) {
-  console.log(buttonPressed);
-  //Here we could get the current string from the html itself, or store in a variable
+
+const decideOperation = function readButtonPressedAndDecideOperation(buttonPressed) {
+  switch (buttonPressed) {
+    // For sum, need to save current value in display, save the operation and wait for another input
+    case '+':
+      break;
+
+    case '-':
+      break;
+
+    case '*':
+      break;
+      
+    case '/':
+      break;
+
+    case 'ac':
+      clearUserInputs();
+      break;
+
+    case '+/-':
+      invertDisplayValue();
+      break;
+
+    case '.':
+      insertDecimalPoint();
+      break;
+    
+    case '=':
+      break;
+
+    default:
+      break;
+  }
+}
+
+const appendNumber = function appendNumberToDisplay(buttonPressed) {
+  if (display !== '' || buttonPressed !== "48") display += String.fromCharCode(buttonPressed); 
+  updateCalculatorDisplay();
+}
+
+const verifyTypeOfInput = function verifyTypeOfButtonPressed(buttonPressed) {
+  isNaN(Number(buttonPressed)) ? decideOperation(buttonPressed) : appendNumber(buttonPressed);
 
 }
 
-//Assigns event listeners to buttons on calculator
+// Assigns event listeners to buttons on calculator
 const buttons = document.querySelectorAll('.key');
 buttons.forEach((button) => {
-  return button.addEventListener('click', () => verifyInput(button.dataset.id));
+  return button.addEventListener('click', () => verifyTypeOfInput(button.dataset.id));
 });
